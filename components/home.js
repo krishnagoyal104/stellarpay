e387d84885551fb2c58028cd6bb7f16e21c09f25
krishnagoyal104 <goyalkrishna104@gmail.com>
2019-03-16 14:02:09 +0530

    Updated HomeView and connected redux

--- components/home.js
+++ components/home.js
@@ -1,10 +1,65 @@
 import React from 'react';
 import {View, Text, Image, StyleSheet, Dimensions} from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 
 const HomeView = (props) => {
   return(	
-	  <Text>Welcome to the Home Screen</Text>
-  );		
-} 
+ 	<View style={styles.mainContainer}>
+ 		<View style={styles.container1}>
+ 		<View style={styles.innerContainers}>
+ 			<Text style={styles.text}>Send</Text>
+ 		</View>
+ 		<View style={styles.innerContainers}> 		
+ 			<Text style={styles.text}>Receive</Text>
+ 		</View>	
+ 		</View>	
+	 	<View style={styles.container2}></View>
+ 	</View>
+  );
+}
+
+const styles = StyleSheet.create({
+	mainContainer: {
+		flex: 1
+	},
+	container1: {
+		flex: 2,
+		flexDirection: 'row',
+		backgroundColor: '#007ee5',
+		alignItems: 'center',
+		justifyContent: 'space-evenly'
+	},
+	container2: {
+		flex: 1
+	},
+	innerContainers: {
+		height: 35,
+		width: 90,
+		alignItems: 'center',
+		justifyContent: 'center',
+		backgroundColor: 'white',
+		borderRadius: 8
+	},
+	text: {
+		fontSize: 16,
+		color: '#007ee5',
+		/*paddingTop: 8,
+		paddingLeft: 25,
+		paddingRight: 25,
+		paddingBottom: 8,
+		alignItems: 'center',
+		borderRadius: 10,
+		backgroundColor: 'white'*/
+	},
+	icon: {
+		padding: 8,
+		borderRadius: 50,
+		backgroundColor: 'white',
+		justifyContent: 'flex-start'
+		//alignItems: 'center'
+	}
+});
+
+
 
 export default HomeView;
--- screens/home.js
+++ screens/home.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import {View, StyleSheet} from 'react-native';
+import {connect} from 'react-redux';
 import {Navigation} from 'react-native-navigation';   
 import HomePage from '../components/home';
+import {getBalance} from '../actions/balance';
 
 class HomeScreen extends React.Component {
 
   constructor(props){
     super(props);
+  }
+
+  componentWillMount(){
+    this.props.dispatch(getBalance());
   }
 
   render() {
@@ -18,5 +24,11 @@ class HomeScreen extends React.Component {
 
 }  
   
-export default HomeScreen;
+const mapStateToProps = (state) => {
+	return{
+		balances: state.balances
+	}
+};  
+
+export default connect(mapStateToProps)(HomeScreen);
 
