acf1efa505a86f6ee267a003d491f9d9c090ecd2
goyalkrishna104 <goyalkrishna104@gmail.com>
2019-04-30 18:40:04 +0530

    Updated signup screen UI

--- components/signup.js
+++ components/signup.js
@@ -1,7 +1,9 @@
 import React from 'react';
-import {View, Text, TextInput, Image, StyleSheet, Button, TouchableOpacity, ActivityIndicator} from 'react-native';
+import {View, Text, TextInput, Image, StyleSheet, Button, TouchableOpacity, ActivityIndicator, Dimensions} from 'react-native';
 import {signUp} from '../actions/resolve';
 import Font from 'react-native-vector-icons/FontAwesome';
+import Icon from 'react-native-vector-icons/Feather';
+//import Ant from 'react-native-vector-icons/AntDesign';
 
 class SignupView extends React.Component{
 
@@ -32,22 +34,31 @@ class SignupView extends React.Component{
 	render(){
 		return(	
 		 	<View style={styles.mainContainer}>
-		 		<View style={styles.titleContainer}>
-		 			<Text style={styles.title}>Welcome to StellarPay</Text>
-		 		</View>
-			 	<View style={styles.inputContainer}>
-			 		<TextInput style={styles.input} placeholder="Enter Name" onChangeText={this.onNameChange}/>
-			 		<TextInput style={styles.input} placeholder="Enter Number" onChangeText={this.onNumberChange}
-			 		keyboardType="numeric"/>
-			 	</View>
-			 	<View style={styles.bottomContainer}>
-			 		<TouchableOpacity style={styles.loginContainer} onPress={() => this.onSignUp()} >
-			  			<View style={{flexDirection: 'row', justifyContent: 'flex-start'}}>
-								<Text style={styles.bottomText}>Sign Up</Text>
-								{this.props.loading ? <ActivityIndicator size="small" color="#007ee5" /> : <Font name={'send'} size={20} color={'#007ee5'} style={{paddingTop: 4, paddingLeft: 6}} />}
-							</View>
+		 		<View style={styles.box}>
+			 		<View style={styles.topContainer}>
+			 			<Text style={styles.title}>Sign Up</Text>
+			 		</View>
+				 	<View style={styles.container}>
+					 	<View style={styles.inputContainer}>
+					 		<Icon name={"user"} size={25} />
+					 		<TextInput style={styles.input} placeholder="Enter Name" onChangeText={this.onNameChange}/>
+					 	</View>
+					 	<View style={styles.inputContainer}>	
+					 		<Icon name={"phone"} size={25} />
+					 		<TextInput style={styles.input} placeholder="Enter Number" onChangeText={this.onNumberChange}
+					 		keyboardType="numeric"/>
+					 	</View>
+				 	</View>
+			 		<TouchableOpacity style={styles.signupContainer} onPress={() => this.onSignUp()} >
+		  			<View style={{flexDirection: 'row', justifyContent: 'flex-start'}}>
+							<Text style={styles.bottomText}>Sign Up</Text>
+							{this.props.loading ? <ActivityIndicator size="small" color="white" /> : <Font name={'send'} size={20} color={'white'} style={{paddingTop: 4, paddingLeft: 6}} />}
+						</View>
 					 </TouchableOpacity>
 			 	</View>
+			 	<View style={styles.iconContainer}>
+		 			<Icon name={"user"} size={50} color={"black"} />
+		 		</View>
 		 	</View>
   	);
 	}
@@ -57,51 +68,69 @@ class SignupView extends React.Component{
 const styles = StyleSheet.create({
 	mainContainer: {
 		flex: 1,
-		backgroundColor: '#007ee5'
+		backgroundColor: '#E3E9ED'
 	},
-	titleContainer: {
-		flex: 4,
+	iconContainer: {
+		height: Dimensions.get('window').height * 10/100,
+		width: Dimensions.get('window').height * 10/100,
+		marginTop: Dimensions.get('window').height * 5/100,
+		position: 'absolute',
+		alignSelf: 'center',
+		justifyContent: 'center',
+		alignItems: 'center',
+		borderRadius: Dimensions.get('window').height * 5/100,
+		backgroundColor: 'white'
+	},
+	box: {
+		height: '60%',
+		width: '80%',
+		marginTop: Dimensions.get('window').height * 10/100,
+		justifyContent: 'space-around',
+		alignSelf: 'center',
+		backgroundColor: 'white'
+	},
+	topContainer: {
+		height: '20%',
 		justifyContent: 'center',
 		alignItems: 'center'
 	},
 	title: {
-		color: 'white',
-		fontSize: 30,
-		fontFamily: 'serif'
+		fontSize: 24,
+		color: 'black'
+	},
+	container: {
+		height: '40%',
+		justifyContent: 'space-around',
+		alignItems: 'center',
 	},
 	inputContainer: {
-		flex: 2,
-		justifyContent: 'space-around',
-		alignItems: 'center'
+		width: '80%',
+		borderWidth: 1,
+		borderColor: '#E3E9ED',
+		flexDirection: 'row',
+		justifyContent: 'flex-start',
+		alignItems: 'center',
+		paddingLeft: 10,
+		borderRadius: 12,
+		backgroundColor: 'white',
 	},
 	input: {
-		backgroundColor: 'white',
-		width: '80%',
 		height: 50,
 		fontSize: 20,
-		borderRadius: 25,
 		borderColor: 'white',
-		paddingLeft: 25
+		paddingLeft: 10
 	},
-	t: {
-		fontSize: 22,
-		fontFamily: 'serif'
-	},
-	bottomContainer: {
-		flex: 2,
-		justifyContent: 'center',
-		alignItems: 'center'
-	},
-	loginContainer: {
+	signupContainer: {
+		height: '15%',
 		width: '80%',
-		height: '35%',
-		borderRadius: 25,
-		backgroundColor: 'white',
+		alignSelf: 'center',
+		borderRadius: 12,
+		backgroundColor: '#007ee5',
 		justifyContent: 'center',
 		alignItems: 'center'
 	},
 	bottomText: {
-		color: '#007ee5',
+		color: 'white',
 		fontSize: 20
 	}
 });
