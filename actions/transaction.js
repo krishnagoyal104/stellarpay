1b87cca31ba538519da1d90a366d71af3ad686d3
goyalkrishna104 <goyalkrishna104@gmail.com>
2019-06-06 14:34:30 +0530

    Updated loading indicator functions
    Implemented separate states for all actions.

--- actions/transaction.js
+++ actions/transaction.js
@@ -1,23 +1,25 @@
 import axios from 'axios';
 import {transact} from '../utils/transaction';
 
-export const uiStartLoading = () => {
+export const uiStartLoading = (context) => {
   return {
     type: 'UI_START_LOADING',
+    context
   };
 };
 
-export const uiStopLoading = () => {
+export const uiStopLoading = (context) => {
   return {
     type: 'UI_STOP_LOADING',
+    context
   };
 };
 
 export const createTransaction = (_receiverPublicKey, _amount, _function, _code, _issuer) => {
   return (dispatch, getState) => {
     dispatch(uiStartLoading());
-  	const publicKey = getState().account.publicKey;
-  	const secretKey = getState().account.secretKey;
+    const publicKey = getState().account.publicKey;
+    const secretKey = getState().account.secretKey;
     const promise = transact(publicKey, secretKey, _receiverPublicKey, _amount, _code, _issuer);
     promise.then((hash) => {
       dispatch(uiStopLoading());
@@ -28,4 +30,4 @@ export const createTransaction = (_receiverPublicKey, _amount, _function, _code,
       dispatch(uiStopLoading());
     });
   };
-};
+};    
--- reducers/transaction.js
+++ reducers/transaction.js
@@ -1,11 +1,37 @@
-const initialState = false;
+const initialState = {resolve: false, payment: false, balance: false, transaction: false, ledger: false, user: false};
 
-export default(state = initialState, action) => {
-  switch (action.type) {
+export default(state = initialState, {type, context}) => {
+  switch (type) {
     case 'UI_START_LOADING':
-      return true;
+      switch(context) {
+      	case 'resolve':
+      		return {...state, resolve: true};
+      	case 'payment':
+      		return {...state, payment: true};
+      	case 'balance':
+      		return {...state, balance: true};
+      	case 'transaction':
+      		return {...state, transaction: true};	
+      	case 'ledger':
+      		return {...state, ledger: true};
+      	case 'user':
+      		return {...state, user: true};				
+      }
     case 'UI_STOP_LOADING':
-      return false;
+      switch(context) {
+      	case 'resolve':
+      		return {...state, resolve: false};
+      	case 'payment':
+      		return {...state, payment: false};
+      	case 'balance':
+      		return {...state, balance: false};
+      	case 'transaction':
+      		return {...state, transaction: false};	
+      	case 'ledger':
+      		return {...state, ledger: false};	
+      	case 'user':
+      		return {...state, user: false};			
+      }
     default:
       return state;  
   };
