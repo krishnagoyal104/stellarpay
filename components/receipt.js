13091cf8c7b10918363689283e4523a11bb224fd
goyalkrishna104 <goyalkrishna104@gmail.com>
2019-06-12 23:37:14 +0530

    Improved Receipt Screen UI

--- components/receipt.js
+++ components/receipt.js
@@ -9,38 +9,52 @@ const ReceiptView = (props) => {
 	const animation = status ? 'tick.json' : 'error.json';
   return(	
 		<View style={styles.mainContainer}>
-			<View>
-				<View style={styles.container}>
-					<Text style={styles.font1}>{status ? 'Paid Successfully' : 'Transaction failed'}</Text>
-					{status ? <LottieView style={styles.animation} source={require(`../static/success.json`)} autoPlay loop={false} /> :
-					<LottieView style={styles.animation} source={require(`../static/error.json`)} autoPlay loop={false} />}
+			<View style={styles.container}>
+				<View>
+					<View style={styles.header}>
+						<Text style={styles.font1}>{status ? 'Paid Successfully' : 'Transaction failed'}</Text>
+						{status ? <LottieView style={styles.animation} source={require(`../static/success.json`)} autoPlay loop={false} /> :
+						<LottieView style={styles.animation} source={require(`../static/error.json`)} autoPlay loop={false} />}
+					</View>
+					<Text style={styles.font1}>{props.amount} {props.code || 'Lumens'}</Text>
 				</View>
-				<Text style={styles.font1}>{props.amount} {props.code || 'Lumens'}</Text>
-			</View>
-			<View>
-				<Text style={styles.font2}>{props.recipient.name.toUpperCase()}'s wallet linked to</Text>
-				<Text style={styles.font1}>{props.recipient.number}</Text>
-			</View>
-			<View>
+				<View>
+					<Text style={styles.font2}>{props.recipient.name.toUpperCase()}'s wallet linked to</Text>
+					<Text style={styles.font1}>{props.recipient.number}</Text>
+				</View>
 				<Text style={styles.font2}>{moment().format('LLL')}</Text>
-				<Text style={styles.font2}>{status && 'Transaction Id:'}</Text>
-				{status ? <Text style={styles.font2} numberOfLines={1} ellipsizeMode={'tail'} selectable={true}>{props.id}</Text> :
-				<Text style={styles.font3}>{props.id}</Text>}
-			</View>
-			<TouchableOpacity style={styles.navigateButton} onPress={() => props.navigate()}>
-					<Icon name={'home'} size={20} color={'#007ee5'} style={{paddingTop: 4, paddingRight: 6}} />
-					<Text style={styles.home}>Home</Text>
-			</TouchableOpacity>
+				<View>
+					<Text style={styles.font2}>{status && 'Transaction Id:'}</Text>
+					{status ? <Text style={styles.font2} numberOfLines={1} ellipsizeMode={'tail'} selectable={true}>{props.id}</Text> :
+					<Text style={styles.font3}>{props.id}</Text>}
+				</View>
+				<TouchableOpacity style={styles.navigationButton} onPress={() => props.navigate()}>
+						<Icon name={'home'} size={20} color={'#007ee5'} style={{paddingTop: 4, paddingRight: 6}} />
+						<Text style={styles.home}>Home</Text>
+				</TouchableOpacity>
+			</View>	
 		</View>
   );	 	
 }
 
 const styles = StyleSheet.create({
 	mainContainer: {
+		flex: 1,
+		justifyContent: 'center',
+		alignItems: 'center'
+	},
+	container: {
 		height: '80%',
+		width: '90%',
 		paddingLeft: 25,
 		paddingRight: 25,
 		justifyContent: 'space-around',
+		elevation: 2,
+		backgroundColor: 'white'
+	},
+	header: {
+		flexDirection: 'row',
+		alignItems: 'center'
 	},
 	font1: {
 		fontSize: 22,
@@ -55,15 +69,11 @@ const styles = StyleSheet.create({
 		fontSize: 20,
 		color: 'black'
 	},
-	container: {
-		flexDirection: 'row',
-		alignItems: 'center',
-	},
 	animation: {
 		height: 70,
 		width: 70,
 	},
-	navigateButton: {
+	navigationButton: {
 		flexDirection: 'row',
 		justifyContent: 'flex-start',
 		alignSelf: 'center'
